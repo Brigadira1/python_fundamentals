54f42b7930b1e0e94a59d60cd06e0dfcbd6d1f80
Brigadira1 <iavor.stoimenov@yahoo.com>
2023-02-09 07:45:54 +0200

    Second commit

---
+++ best_practices/lists/ListCheatSheet.py
@@ -0,0 +1,6 @@
+"""
+
+The idea behind this file is to present numerous basic examples for manipulating lists in Python
+
+"""
+
--- exercises/Teeeeeeeeeeeest.py
+++ exercises/Teeeeeeeeeeeest.py
@@ -1,14 +1,1 @@
 
-
-
-a = input("Enter a string: ")
-b = input("Enter another string: ")
-
-count = 0
-
-
-for i in range(len(a) - 1):
-    if (b == a[i: i + len(b)]):
-        count += 1
-
-print(count)
