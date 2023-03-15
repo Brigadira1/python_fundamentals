a3182fe2e4314ad6f0ba2ba8fca270ef72793137
Brigadira1 <iavor.stoimenov@yahoo.com>
2023-03-15 19:17:31 +0200

    Insertion sort added

--- Test.py
+++ Test.py
@@ -1,4 +1,32 @@
-list=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+def bubble_sort(list):
 
-print(list.insert(1, 99999))
-print(list)
+    for i in range(0, len(list)):
+        sorted = True
+        for j in range(0, len(list) - i - 1):
+            if list[j] > list[j + 1]:
+                list[j + 1], list[j] = list[j], list[j+1]
+                sorted = False
+
+        if sorted:
+            return list
+
+    return list
+
+# list=[1,2,8,4,5,6,7,2,3,4,5,99, 100, 45]
+# print(bubble_sort(list))
+
+
+def insertion_sort(list):
+    for i in range(1, len(list)):
+        j = i
+
+        while list[j - 1] > list[j] and j > 0:
+            list[j], list[j-1] = list[j - 1], list[j]
+            j -= 1
+
+    return list
+
+
+
+list=[98, 34, 23, 29, 4, 5, 46, 768, 0,]
+print(insertion_sort(list))
--- algorithms/sorting_algorithms/insertion_sort.py
+++ algorithms/sorting_algorithms/insertion_sort.py
@@ -1,4 +1,13 @@
 def insertion_sort(items):
     for i in range(1, len(items)):
-        for j in range(i, -1, -1):
-            if items[i] < items[j-1]:
+        j = i
+        while items[j - 1] > items[j] and j > 0:
+            items[j], items[j - 1] = items[j - 1], items[j]
+            j -= 1
+
+    return items
+
+
+items = [10, 11, 3, 4, 5, 2, 1,1000, 900, 3, 4, 2, 1]
+
+print(insertion_sort(items))
