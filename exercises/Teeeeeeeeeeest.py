412ac76b66ed0b5e409878cda607e9fafae9ea86
brigadira1 <iavor.stoimeonv@yahoo.com>
2024-10-20 22:45:41 +0300

    First attempt for multithreaded server-client chat

--- exercises/Teeeeeeeeeeest.py
+++ exercises/Teeeeeeeeeeest.py
@@ -1,5 +1,9 @@
-l_ist = [i for i in range(10)]
-l_ist1 = list("WXYZ")
-l_ist2 = [i * i for i in range(10)]
-combined_list = l_ist + l_ist2 + l_ist1
-print(combined_list)
+def test():
+    try:
+        print("In the function")
+        return 5
+    finally:
+        print("In the finally block")
+
+
+print(test())
---
+++ exercises/sockets/server.py
@@ -0,0 +1,59 @@
+import socket
+import threading
+
+
+class Server:
+    def __init__(
+        self, server_address: str = "127.0.0.1", server_port: int = 9999
+    ) -> None:
+        self.server_address = server_address
+        self.server_port = server_port
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket.bind((self.server_address, self.server_port))
+        self.clients = []
+        self.aliases = []
+
+    def run_server(self) -> None:
+        self.server_socket.listen()
+        print(f"Listening on {self.server_address}:{self.server_port}")
+
+        while True:
+            client_socket, client_address = self.server_socket.accept()
+            print(f"Connection acceppted from {client_address[0]}:{client_address[1]}")
+            client_socket.send(
+                "Connection with the server successfully established\n".encode("utf-8")
+            )
+            client_socket.send("ALIAS:".encode("utf-8"))
+            alias = client_socket.recv(1024).decode("utf-8")
+            self.broadcast(f"{alias} joined the conversation".encode("utf-8"))
+            self.clients.append(client_socket)
+            self.aliases.append(alias)
+            t = threading.Thread(target=self.handle_client, args=(client_socket,))
+            t.start()
+
+    def broadcast(self, message):
+        for client_socket in self.clients:
+            client_socket.send(message)
+
+    def handle_client(self, client_socket):
+        while True:
+            try:
+                message = client_socket.recv(1024)
+                self.broadcast(message)
+            except Exception as e:
+                index = self.clients.index(client_socket)
+                self.broadcast(
+                    f"{self.aliases[index]} was removed from the conversaiton.".encode(
+                        "utf-8"
+                    )
+                )
+                self.clients.remove(index)
+                self.aliases.remove(index)
+                break
+            finally:
+                client_socket.close()
+
+
+if __name__ == "__main__":
+    server = Server()
+    server.run_server()
