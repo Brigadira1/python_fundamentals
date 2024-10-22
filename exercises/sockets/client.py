db24b372ad354ae7a14e29d6ccac13f8c5818313
brigadira1 <iavor.stoimeonv@yahoo.com>
2024-10-22 12:33:06 +0300

    Final fixes for multithreading related to client-server

--- exercises/sockets/client.py
+++ exercises/sockets/client.py
@@ -1,4 +1,5 @@
 import socket
+import sys
 import threading
 
 
@@ -6,37 +7,47 @@ class Client:
     def __init__(
         self, server_address: str = "127.0.0.1", server_port: int = 9999
     ) -> None:
-        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client_socket: socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.client_socket.connect((server_address, server_port))
+        self.alias = None
 
-    def receive(self):
+    def receive(self, event) -> None:
         while True:
             try:
-                message = self.client_socket.recv(1024).decode("utf-8")
-                if message == "ALIAS:":
-                    alias = input("Enter your alias: ")
-                    self.client_socket.send(alias.encode("utf-8"))
-                print(message)
-            except Exception as e:
-                print("Connection is dropped from server side")
-                self.client_socket.close()
+                message: str = self.client_socket.recv(1024).decode("utf-8")
+                # print(f"Raw message received: {repr(message)}")
+                if "ALIAS" in message:
+                    al = input("Enter your alias: ")
+                    self.alias = al
+                    self.client_socket.send(self.alias.encode("utf-8"))
+                    event.set()
+                else:
+                    # event.set()
+                    print(message)
+            except Exception() as e:
+                print(f"Connection is dropped from server side: {e}")
                 break
+        self.client_socket.close()
+        sys.exit(0)
 
-    def send(self):
+    def send(self, event) -> None:
+        event.wait()
         while True:
             try:
-                self.client_socket.send(f"{input()}".encode("utf-8"))
-            except Exception as e:
-                print("Error communicating with the server")
-                self.client_socket.close()
+                message: str = str(input())
+                self.client_socket.send(f"{message}".encode("utf-8"))
+            except Exception() as e:
+                print(f"Error communicating with the server: {e}")
                 break
+        self.client_socket.close()
+        sys.exit(0)
 
 
 if __name__ == "__main__":
     client = Client()
 
-    receive_thread = threading.Thread(target=client.receive, args=())
-    sending_thread = threading.Thread(target=client.send, args=())
-
+    event = threading.Event()
+    receive_thread = threading.Thread(target=client.receive, args=(event,))
     receive_thread.start()
+    sending_thread = threading.Thread(target=client.send, args=(event,))
     sending_thread.start()
