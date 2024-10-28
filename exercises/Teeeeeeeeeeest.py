5f7ed12f494458d1610e678c3cb999f800e2208e
brigadira1 <iavor.stoimeonv@yahoo.com>
2024-10-28 21:56:19 +0200

    additional examples added

--- exercises/Teeeeeeeeeeest.py
+++ exercises/Teeeeeeeeeeest.py
@@ -1,9 +1,17 @@
-def test():
-    try:
-        print("In the function")
-        return 5
-    finally:
-        print("In the finally block")
+a_list = (
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+)
+b_list = (7, 8, 9, 10, 11, 12, 13)
 
 
-print(test())
+dict_1 = {"a": 1, "b": 2}
+dict_2 = {"c": 3, "d": 4}
+
+print(list(zip(a_list, b_list)))
+print(list(zip(dict_1, dict_2)))
+help(readlines)
