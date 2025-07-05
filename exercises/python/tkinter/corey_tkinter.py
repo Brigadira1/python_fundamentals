e0be28fb65cb7acab7a82aa43b69ba77c4f05fa8
brigadira1 <iavor.stoimenov@yahoo.com>
2025-07-05 18:25:11 +0300

    Completely finished corey tkinter example

--- exercises/python/tkinter/corey_tkinter.py
+++ exercises/python/tkinter/corey_tkinter.py
@@ -17,6 +17,9 @@ def add_to_list(_event=None):
 frame = tk.Frame(root)
 frame.grid(row=0, column=0, sticky="nsew")
 
+frame.grid_columnconfigure(0, weight=1)
+frame.grid_rowconfigure(1, weight=1)
+
 entry = tk.Entry(frame)
 entry.grid(row=0, column=0)
 
@@ -26,6 +29,6 @@ button = tk.Button(frame, text="Add", command=add_to_list)
 button.grid(row=0, column=1)
 
 text_list = tk.Listbox(frame)
-text_list.grid(row=1, column=0, columnspan=2, sticky="ew")
+text_list.grid(row=1, column=0, columnspan=2, sticky="nsew")
 
 root.mainloop()
