214d2840b3ddcca59ee3657fb609149e3c58046a
brigadira1 <iavor.stoimenov@yahoo.com>
2025-07-15 17:53:20 +0300

    First phase of password manager is finished

--- tutorials/100 Days Python Bootcamp 2023/Day29/password_manager.py
+++ tutorials/100 Days Python Bootcamp 2023/Day29/password_manager.py
@@ -1,17 +1,43 @@
 from pathlib import Path
-from tkinter import Button, Canvas, Label, PhotoImage, Tk
+from tkinter import Button, Canvas, Label, PhotoImage, Tk, Entry, Frame
 
 root = Tk()
-root.wm_title("Iavor Passord Manager")
-root.configure(width=800, height=800, pady=20)
+root.wm_title("Iavor Password Manager")
+root.configure(padx=20, pady=20)
 
-canvas = Canvas(root, width=400, height=400)
-path: Path = Path(__file__).resolve().parent
-path_to_pgn = path / "logo.png"
-print(path_to_pgn)
-my_pass_img = PhotoImage(file=path_to_pgn, height=200, width=200)
-canvas.create_image(200, 200, image=my_pass_img)
-canvas.pack()
+frame = Frame(root)
+frame.grid(row=0, column=0)
+
+canvas = Canvas(frame, width=200, height=200)
+
+path_to_pgn= Path(__file__).resolve().parent / "logo.png"
+my_pass_img = PhotoImage(file=path_to_pgn)
+canvas.create_image(100, 100, image=my_pass_img)
+canvas.grid(row=0, column=1)
+
+website = Label(frame, text="Website:")
+website.grid(row = 1, column = 0)
+
+email_username = Label(frame, text = "Email/Username:")
+email_username.grid(row=2, column=0)
+
+password = Label(frame, text="Password:")
+password.grid(row=3, column=0)
+
+website_entry = Entry(frame, width=35)
+website_entry.grid(row=1, column=1, columnspan=2, sticky="w")
+
+email_entry = Entry(frame, width=35)
+email_entry.grid(row=2, column=1, columnspan=2, sticky="w")
+
+password_entry = Entry(frame, width=35)
+password_entry.grid(row=3, column=1, sticky="w")
+
+generate_password = Button(frame, text="Generate Password", width=15)
+generate_password.grid(row=3, column=1, sticky="e")
+
+add_button = Button(frame, text="Add", width=29)
+add_button.grid(row=4, column=1, columnspan=2, sticky="w")
 
 
 root.mainloop()
